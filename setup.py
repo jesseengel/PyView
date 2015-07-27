c87180bb87b99791ab0a363e8445f6eb70a804cd
jesseengel <jesse.engel@gmail.com>
2015-07-26 18:53:30 -0700

    version bump for schoolboy mistake

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
 
     description='A Simple GUI creator for python using wxpython. '
     'Combined with numpy, meant as an open source labview replacement.',
